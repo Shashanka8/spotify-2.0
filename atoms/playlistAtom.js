b5fafb645d17f6d82690497f036e9265d4daed55
Shashanka8 <nayak.shashanka97@gmail.com>
2022-02-05 21:21:20 +0530

    Removed TS setup, UI-Added sidebar with menu & playlists, used Nextauth with Spotify web API for authentication

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const playlistState = atom({
+  key: 'playlistState',
+  default: null,
+});
+
+export const playlistIdState = atom({
+  key: 'playlistIdState',
+  default: '6P6P23Jhj5OX6OJdtbhq4y',
+});
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Center from '../components/Center';
+import Sidebar from '../components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className='h-screen overflow-hidden bg-black'>
+      <main className='flex'>
+        <Sidebar />
+        <Center />
+      </main>
+
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
