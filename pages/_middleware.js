fcd7d1fe6fe3d3a54bbe78f24b9927bfec1654ec
Shashanka8 <nayak.shashanka97@gmail.com>
2022-02-06 20:38:43 +0530

    modified middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,16 +3,20 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   // Token will extst if user is logged in
-  const token = await getToken({ req, secret: process.env.JWt_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.JWt_SECRET,
+    raw: true,
+  });
 
   const { pathname } = req.nextUrl;
 
   console.log(pathname, token);
 
   // if user is already signedin, but goes to login page, redirect to home page
-  // if (token && pathname === '/login') {
-  //   return NextResponse.redirect('/');
-  // }
+  if (token && pathname === '/login') {
+    return NextResponse.redirect('/');
+  }
 
   // Allow the requests if the following is true...
   // 1) Its a request for next-auth session & provider fetching
@@ -22,7 +26,7 @@ export async function middleware(req) {
   }
 
   // Redirect them to login if they dont have token AND are requesting a protected route
-  // if (!token && pathname !== '/login') {
-  //   return NextResponse.redirect('/login');
-  // }
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
 }
