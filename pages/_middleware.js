585900ab26f3009bb4785b7be5c8196367149de0
Shashanka8 <nayak.shashanka97@gmail.com>
2022-02-06 19:45:09 +0530

    modified middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -20,7 +20,7 @@ export async function middleware(req) {
   }
 
   // Redirect them to login if they dont have token AND are requesting a protected route
-  if (!token && pathname !== '/login') {
-    return NextResponse.redirect('/login');
-  }
+  // if (!token && pathname === '/') {
+  //   return NextResponse.redirect('/login');
+  // }
 }
